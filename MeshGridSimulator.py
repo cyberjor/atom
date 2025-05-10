2225a70eefea6fe8355e2d7d1590beb1e58f5b47
cyberjor <jordanmstephens@gmail.com>
2025-05-09 23:45:23 -0400

    Update MeshGridSimulator.py

--- MeshGridSimulator.py
+++ MeshGridSimulator.py
@@ -134,4 +134,3 @@ if frequency_shift > 0:
     st.warning("System is overloaded — frequency drop may cause instability!")
 if any(ld["Voltage (V)"] < V_NOMINAL for ld in load_data):
     st.warning("One or more inverters are voltage sagging to meet power limits!")
-
